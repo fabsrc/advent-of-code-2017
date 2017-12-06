edc9b6562899e5e119f802332be17e185630a09a
Fabian Schneider <fabbbbbi+git@googlemail.com>
2017-12-06 21:43:53 +0100

    :christmas_tree::calendar: 06

---
+++ 06/two.rb
@@ -0,0 +1,23 @@
+def reallocate(input)
+    memory = input.split.map(&:to_i)
+    store = {}
+    count = 0
+
+    until store[memory.hash] do
+        store[memory.hash] = count
+        count += 1
+        max = memory.max
+        idx = memory.index(max)
+        memory[idx] = 0
+
+        max.times do
+            idx = idx < memory.size - 1 ? idx + 1 : 0
+            memory[idx] += 1
+        end
+    end
+    
+    count - store[memory.hash]
+end
+
+puts(reallocate("0 2 7 0") == 4)
+puts(reallocate("4	10	4	1	8	4	9	14	5	1	14	15	0	15	3	5"))
